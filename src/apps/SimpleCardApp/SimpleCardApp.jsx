8e63981914986cba8286d678a2dfdb3261168183
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-05-08 15:55:20 -0600

    Finish P1_nested components - simple card

---
+++ src/apps/SimpleCardApp/SimpleCardApp.jsx
@@ -0,0 +1,20 @@
+import SimpleCard from "../../components/SimpleCard";
+import "./SimpleCardApp.css"
+
+const SimpleCardApp = () => {
+    const item = {
+        imgSrc: "https://cdn-icons-png.flaticon.com/512/1163/1163142.png?w=826&t=st=1683570111~exp=1683570711~hmac=38277440edceeb97d2543164625c477a92e9dfff48980b4a763d795b7175a52e",
+        alt: "character profile",
+        title: "Ninja Prince",
+        desc: "Born in 1523 A.D., Ninja Prince has always been a long time royal ninja as well as a royal prince. His specialty moves include submit no jutsu and loyalty storm."
+    };
+
+    return (
+        <div id="SimpleCardApp">
+            <h1>Simple Card</h1>
+            <SimpleCard item={item} />
+        </div>
+    );
+}
+
+export default SimpleCardApp;
