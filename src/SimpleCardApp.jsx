efcca4dbc2fbd5aa72d0d06f9dbac964027b2ad6
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-05-08 11:40:53 -0600

    Init start- simple version

---
+++ src/SimpleCardApp.jsx
@@ -0,0 +1,17 @@
+const SimpleCardApp = () => {
+    return (
+        <div id="SimpleCardApp">
+            <div className="img-container">
+                <img src="" alt="none" />
+            </div>
+            <div className="text-box">
+                <h1 className="title">A Title</h1>
+                <p className="description">
+                    The description goes here.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default SimpleCardApp;
